b9cbec2eb97fb660d5f207bc8b8055094e525db4
Andrei Buhus <andrus_sheva@yahoo.com>
2020-02-12 15:31:41 -0800

    graph

--- src/routes/blog/index.svelte
+++ src/routes/blog/index.svelte
@@ -1,6 +1,38 @@
 <script>
-  import Social from "../../components/Global/Social.svelte";
+  import ApolloClient from "apollo-boost";
+  import { setClient, getClient, query } from "svelte-apollo";
+  import { gql } from "apollo-boost";
+
+  const client = new ApolloClient({
+    uri: "https://todos-graphql.herokuapp.com/graphql"
+  });
+  setClient(client);
+
+  const GET_TODOS = gql`
+    {
+      getTodos {
+        id
+        text
+        done
+      }
+    }
+  `;
+
+  const newClient = getClient();
+
+  const todos = query(newClient, { query: GET_TODOS });
 </script>
 
 <h1>blog</h1>
-<Social />
+
+{#await $todos}
+  Loading...
+{:then result}
+  {#each result.data.getTodos as todo}
+    <ul>
+      <li>{todo.text}</li>
+    </ul>
+  {/each}
+{:catch error}
+  {error}
+{/await}
