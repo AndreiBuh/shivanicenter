a334e37041a3501edfb246d6e5caaacc0e3a0cdf
Andrei <andrei.buhus@yahoo.com>
2020-04-23 02:40:30 +0300

    share

--- src/routes/articole/dezvoltare-personala/[slug].svelte
+++ src/routes/articole/dezvoltare-personala/[slug].svelte
@@ -12,8 +12,167 @@
 </script>
 
 <script>
-  import ArticlesSlugs from "../../../components/Articles/ArticlesSlugs.svelte";
+  import ArticlesSidebar from "../../../components/Articles/ArticlesSidebar.svelte";
+  import ShareSocialIcons from "../../../components/Global/ShareSocialIcons.svelte";
   export let article;
+
+  let {
+    title,
+    image,
+    avatar,
+    author,
+    published,
+    content,
+    category,
+    slug,
+    likes,
+    category_slug,
+    id,
+    excerpt
+  } = article[0];
 </script>
 
-<ArticlesSlugs {article} />
+<style>
+  h3 {
+    font-family: var(--font-main), cursive;
+  }
+
+  .card-img-top {
+    width: 100%;
+    border-radius: calc(0.25rem - 1px);
+  }
+
+  .avatar {
+    display: block;
+    width: 100px;
+    margin-top: -60px;
+    overflow: hidden;
+  }
+
+  .avatar img {
+    width: 100%;
+    background: none repeat scroll 0 0 #fff;
+    border: 2px solid #fff;
+  }
+
+  .details {
+    color: var(--main-bg-color);
+    font-size: 14px;
+    text-transform: uppercase;
+    letter-spacing: 1px;
+    font-family: var(--font-main), sans-serif;
+  }
+
+  a {
+    text-decoration: none;
+    font-size: 12px;
+    color: var(--category-color);
+    font-family: var(--font-main);
+    letter-spacing: 2px;
+    text-transform: uppercase;
+  }
+  /* Articole pages style START */
+  .article-card-body {
+    width: 90%;
+    margin: 0 auto;
+  }
+
+  .article-card-body :global(p, li) {
+    font-family: var(--font-text), sans-serif;
+    font-size: 18px;
+    line-height: 22px;
+  }
+
+  .article-card-body :global(h1, h2, h3, h4, h5, h6, h7) {
+    font-family: var(--font-main), sans-serif;
+  }
+
+  .article-card-body :global(div) {
+    padding: 10px;
+  }
+
+  :global(.iframe) {
+    height: 450px;
+    width: 95%;
+  }
+
+  @media screen and (max-width: 768px) {
+    .article-card-body {
+      width: 95%;
+    }
+
+    .article-card-body :global(p, li) {
+      font-size: 16px;
+      line-height: 20px;
+    }
+
+    :global(.iframe) {
+      height: 200px;
+    }
+  }
+
+  /* Articole pages style END */
+</style>
+
+<div class="container">
+  <div class="row">
+    <div class="col-md-8 mb-3">
+      <div class="card">
+        <h3 class="text-center p-3 my-0">{title}</h3>
+        <div class="card-content">
+          <div class="view view-cascade overlay">
+            <img class="card-img-top" src={image.url} alt="Educatie fete" />
+          </div>
+          <div class="avatar mx-auto white">
+            <img
+              src={avatar.url}
+              class="rounded-circle img-fluid"
+              alt="First sample avatar image" />
+          </div>
+          <div class="text-center">
+            <div class="details">{author} | {published}</div>
+            <div class="category">
+              <a href="/articole/{category_slug}" class="category-link">
+                {category.title}
+              </a>
+            </div>
+            <div
+              class="fb-share-button"
+              data-href="https://www.shivanicenter.ro/articole/{category_slug}/{slug}"
+              data-layout="button_count"
+              data-size="small">
+              <a
+                target="_blank"
+                href="https://www.facebook.com/sharer/sharer.php?u=https%3A%2F%2Fwww.shivanicenter.ro%2Farticole%2Fdezvoltare-personala%2F{slug}&amp;src=sdkpreparse"
+                class="fb-xfbml-parse-ignore">
+                Share
+              </a>
+            </div>
+            <!-- <ShareSocialIcons
+              {likes}
+              {id}
+              {category_slug}
+              {slug}
+              on:like
+              {image} /> -->
+          </div>
+        </div>
+        <div class="card-body article-card-body">
+          {@html content}
+        </div>
+      </div>
+    </div>
+    <div class="col-md-4">
+      <ArticlesSidebar />
+    </div>
+  </div>
+</div>
+
+<svelte:head>
+  <meta property="og:title" content={title} />
+  <meta
+    property="og:url"
+    content="https://www.shivanicenter.ro/articole/{category_slug}/{slug}" />
+  <meta property="og:description" content={excerpt} />
+  <meta property="og:image" content={image.url} />
+</svelte:head>
