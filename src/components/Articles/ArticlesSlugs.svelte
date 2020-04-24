154f1bcef3d9630ac34af8b90270a654b6313fd9
Andrei <andrei.buhus@yahoo.com>
2020-04-24 21:13:32 +0300

    share linkedin

--- src/components/Articles/ArticlesSlugs.svelte
+++ src/components/Articles/ArticlesSlugs.svelte
@@ -179,7 +179,13 @@
                   </div>
                 </li>
                 <li>
-                  <a href="www.linkedin.com" rel="noopener">
+                  <a
+                    href="https://www.linkedin.com/shareArticle?mini=true&url=https://www.shivanicenter.ro/articole/{category_slug}/{slug}&title=CreateLinkedIn
+                    Share button on Website
+                    Webpages&summary=chillyfacts.com&source=Chillyfacts"
+                    onclick="window.open(this.href, 'mywin',
+                    'left=20,top=20,width=500,height=500,toolbar=1,resizable=0');
+                    return false;">
                     <i class="fa fa-linkedin" aria-hidden="true" />
                   </a>
                 </li>
